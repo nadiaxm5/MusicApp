9958bcc93c028de0a4bc98071ee6909760858168
Namomi5 <90263390+nadiaxm5@users.noreply.github.com>
2022-05-15 20:05:37 +0200

    network and song

--- app/src/main/java/es/uji/al394716/musicapp/Entities/Song.kt
+++ app/src/main/java/es/uji/al394716/musicapp/Entities/Song.kt
@@ -1,4 +1,33 @@
 package es.uji.al394716.musicapp.Entities
 
-class Song {
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.PrimaryKey
+
+class Song(@PrimaryKey val idSong : Int,
+           val title:String?): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeInt(idSong)
+        p0.writeString(title)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Song> {
+        override fun createFromParcel(parcel: Parcel): Song {
+            return Song(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Song?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
--- app/src/main/java/es/uji/al394716/musicapp/model/Network.kt
+++ app/src/main/java/es/uji/al394716/musicapp/model/Network.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import es.uji.al394716.musicapp.Entities.Song
+import org.json.JSONException
+import org.json.JSONObject
 import javax.xml.transform.ErrorListener
 
 private const val HOST_HEADER_NAME = "X-RapidAPI-Host"
@@ -14,38 +17,46 @@ private const val KEY_HEADER_NAME = "X-RapidAPI-Key"
 private const val KEY_HEADER = "be915f27a0mshd8a5e73640133f3p12645djsn77b9048a7d55"
 private const val HOST_HEADER = "genius.p.rapidapi.com"
 
-private const val SONGS_URL = "https://genius.p.rapidapi.com/songs/"
+private const val SONG_URL = "https://genius.p.rapidapi.com/songs/"
+
+private const val NAME = "title"
+private const val ID = "id"
+private const val SONG_ARRAY = "song"
+//song id that actually works 3039923
 
 class Network private constructor(context: Context){
 
     companion object: SingletonHolder<Network, Context>(::Network)
     val queue = Volley.newRequestQueue(context)
 
-    /*
-    fun getSongs(listener: Response.Listener<List<Song>>, errorListener: Response.ErrorListener){
-        val urlSongs = "$SONGS_URL"
-        Log.d("Alcoholico", "EL URL ES $urlCategories")
-        val jsonObjectRequestCategories = JsonObjectRequest(
-            Request.Method.GET, urlCategories, null,
-            { response -> processCategories(response, listener, errorListener) },
+    fun getSongWithId(idSong: Int, listener: Response.Listener<Song>, errorListener: Response.ErrorListener){
+        val urlSong = "$SONG_URL$idSong"
+        Log.d("Genius", "EL URL ES $urlSong")
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, urlSong, null,
+            { response -> processSongWithId(response, listener, errorListener) },
             { error -> errorListener.onErrorResponse(error) })
-        queue.add(jsonObjectRequestCategories)
+             fun getHeaders(): MutableMap<String, String> {                                         //override!!
+                val headers = HashMap<String, String>()
+                headers[HOST_HEADER_NAME] = HOST_HEADER
+                headers[KEY_HEADER_NAME] = KEY_HEADER
+                return headers
+            }
+        queue.add(jsonObjectRequest)
     }
 
-    val stringRequest = object: StringRequest(Request.Method.GET, linkTrang,
-        Response.Listener<String> { response ->
-            Log.d("A", "Response is: " + response.substring(0,500))
-        },
-        Response.ErrorListener {  })
-    {
-        override fun getHeaders(): MutableMap<String, String> {
-            val headers = HashMap<String, String>()
-            headers["Authorization"] = "Basic <<YOUR BASE64 USER:PASS>>"
-            return headers
+    private fun processSongWithId(response: JSONObject, listener: Response.Listener<Song>, errorListener: Response.ErrorListener) {
+        Log.d("Genius", "PROCESANDO JSON...")
+
+        try {
+            val songArray = response.getJSONArray(SONG_ARRAY)
+            val songObject : JSONObject = songArray[0] as JSONObject
+            val songId = songObject.getInt(ID)
+            val songName = songObject.getString(NAME)
+            val song = Song(songId, songName)
+            listener.onResponse(song)
+            Log.d("Genius", "Funciona")
+        } catch (e: JSONException){
+            errorListener.onErrorResponse(VolleyError("BAD JSON processing"))
         }
     }
-
-    queue.add(stringRequest)
-    */
-
 }
