bdbc97bafa250312560e8128ecc8b6646b897058
alberto2114 <90263353+alberto2114@users.noreply.github.com>
2022-05-29 11:22:29 +0200

    new layout

---
+++ app/src/main/java/es/uji/al394716/musicapp/Activity3/Activity3.kt
@@ -0,0 +1,12 @@
+package es.uji.al394716.musicapp.Activity3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.uji.al394716.musicapp.R
+
+class Activity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_3)
+    }
+}
